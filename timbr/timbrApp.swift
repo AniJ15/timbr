9b64c345a1803e78e9f875ba5aa027bcc357fdfd
Priya <pg492@cornell.edu>
2025-11-15 00:01:53 -0500

    Initial Commit

---
+++ timbr/timbrApp.swift
@@ -0,0 +1,17 @@
+//
+//  timbrApp.swift
+//  timbr
+//
+//  Created by Priya Gokhale on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct timbrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
