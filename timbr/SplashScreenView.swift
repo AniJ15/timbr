997e0cf44081c86815d50b3aeaded1323cfec3ef
Ani Jain <aj655@cornell.edu>
2025-11-21 23:04:47 -0500

    Added a loading page before the homepage

---
+++ timbr/SplashScreenView.swift
@@ -0,0 +1,47 @@
+//
+//  SplashScreenView.swift
+//  timbr
+//
+//  Created by Priya Gokhale on 11/15/25.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var opacity = 0.0
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Color.timbrDark
+                    .ignoresSafeArea()
+                
+                Image("TimbrLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 180, height: 180)
+                    .opacity(opacity)
+            }
+            .onAppear {
+                withAnimation(.easeIn(duration: 0.6)) {
+                    opacity = 1.0
+                }
+                
+                // Delay before showing main content
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    withAnimation(.easeOut(duration: 0.4)) {
+                        opacity = 0.0
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                        isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
